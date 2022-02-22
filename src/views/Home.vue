28019fccafaf76ea0754e6101ae44d497f7ce6fa
Luiz Junior <jr.luizandrade@gmail.com>
2022-02-22 11:02:59 -0300

    Criando banner do app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,19 @@
 <template>
+<Banner />
   <div class="main-container">
-    <h1>
-      HOME
-    </h1>
+    <h1>Monte o seu Burger:</h1>
+    <p>Form</p>
   </div>
 </template>
 
 <script>
+import Banner from "../components/Banner.vue"
+
+export default{
+  name: "Nome",
+  components: {
+    Banner
+  }
+}
 
 </script>
